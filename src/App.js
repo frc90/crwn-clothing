61d7c076250beb351883f5c4f62da94dd2098eb1
frc90 <francisco.padron86@gmail.com>
2020-10-26 11:04:21 -0700

    6. Master Project Forms + Components 2. Shop Page

--- src/App.js
+++ src/App.js
@@ -1,54 +1,25 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-import { Link, Route, Switch } from "react-router-dom";
-
-const HatsPage = (props) => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
-
-const MansPage = (props) => {
-  return (
-    <div>
-      <h1>MANS PAGE</h1>
-    </div>
-  );
-};
-const WomansPage = (props) => {
-  return (
-    <div>
-      <h1>WOMANS PAGE</h1>
-    </div>
-  );
-};
-
-const JacketsPage = (props) => {
-  return (
-    <div>
-      <h1>JACKETS PAGE</h1>
-    </div>
-  );
-};
+import { Route, Switch } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/jackets" component={JacketsPage} />
-        <Route path="/womans" component={WomansPage} />
-        <Route path="/mans" component={MansPage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
 }
 
 export default App;
+
+// <Route path="/jackets" component={JacketsPage} />
+//         <Route path="/womans" component={WomansPage} />
+//         <Route path="/mans" component={MansPage} />
 
 // <Nav />
 // const Nav = () => {
